1496a40f1ca4456e7279d9519e4e5eae5b25673f
akupreichik <anton.kupreichik@ontravelsolutions.com>
2021-06-11 16:49:52 +0300

    add dockerfile

--- src/main/java/by/jrr/Application.java
+++ src/main/java/by/jrr/Application.java
@@ -2,6 +2,7 @@ package by.jrr;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Start FinalProject");
-    }
+        System.out.println("hello from docker");
+        }
+
 }
